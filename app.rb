b03266b2fc0ee9889f9f12214c901a3597a06403
Jacob Hixon and Kevin Finley <kevin@>
2016-12-13 11:38:59 -0800

    pass Question spec for validating title presence

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require('bundler/setup')
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @surveys = Survey.all
+  erb(:index)
+end
+
+get('/questions') do
+  @questions = Question.all
+  erb(:questions)
+end
+
+get('/surveys') do
+  @surveys = Survey.all
+end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -1,6 +1,11 @@
 require('spec_helper')
 
 describe(Survey) do
+  it('validates the presence of a title') do
+    survey = Survey.new({title: '', description: 'a survey'})
+    expect(survey.save()).to(eq(false))
+  end
+
   describe('#questions') do
     it('returns all questions that belong to the survey') do
       test_survey = Survey.create({title: 'Favorite Butter', description: 'A survey of peoples favorite butter flavors'})
